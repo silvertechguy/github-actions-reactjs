e9e1ef1cf0b0c73801f0288688907b1b0ee1fc2b
Ali Fadda <silvertechguy@gmail.com>
2020-11-01 00:21:25 +0200

    test commit msg

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -17,6 +15,9 @@ function App() {
         >
           Learn React
         </a>
+        <div>
+          <h1>Hello, World!</h1>
+        </div>
       </header>
     </div>
   );
